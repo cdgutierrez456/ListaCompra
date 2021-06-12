3d096d90354d6a2677d5f7a22c3d6c6dbe201b3a
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-06-12 15:23:12 -0500

    Creacion componentes principales y creacion de modelo html en app.component

---
+++ src/app/models/producto.models.ts
@@ -0,0 +1,13 @@
+export class Producto {
+    
+    nombre: string;
+    cantidad: number;
+    departamento: string;
+
+    constructor(pNombre: string, pCantidad: number, pDepartamento: string) {
+        this.nombre = pNombre;
+        this.cantidad = pCantidad;
+        this.departamento = pDepartamento;
+    }
+
+}
